3cd1711c90fbf4cdea5130fb60f5abebb52d0cee
Harsh Bansal <harshofficial0230@gmail.com>
2020-06-06 21:03:52 +0530

    decisionpoint

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { Table, Button } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props){
+    super();
+    this.state = {
+      users : [],
+      users_title :[]
+    }
+  }
+  componentDidMount()
+  {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+  .then( (response)=> {
+    this.setState({users : response.data})
+  })
+  }
+  userList=()=>{
+    return this.state.users.map(urs=>{
+    return (<tr user={urs}    key = {urs.id}>
+    <td>{urs.id}  </td>
+    <td>{urs.name}</td>
+    <td><Button onClick={()=>this.getinfo(urs.id)}>View Info</Button></td>
+    </tr>
+    )
+    })
 }
 
-export default App;
+  getinfo=(id)=>{
+  axios.get('https://jsonplaceholder.typicode.com/posts?userId='+id)
+  .then( (response)=> {
+    this.setState({users_title : response.data})
+   console.log(this.state.users_title);
+   document.innerHTML= this.state.users_title ;
+  })
+}
+  render() {
+    return (
+      <div >
+        <Table>
+              <thead className="thead-dark">
+                <tr>
+                  <th>User ID </th>
+                  <th>Name</th>
+                  <th>More Info</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.userList()}
+              </tbody>
+            </Table>
+      </div>
+    )
+  }
+}
+
